0593638a4e724fa5d482feab16ba6583b36371c1
Argir Argirov <dj.giro@gmail.com>
2017-09-22 13:27:14 -0400

    car class created

---
+++ cat.rb
@@ -0,0 +1,18 @@
+class Cat
+
+  def initialize(name, food, meal_time)
+    @name = name
+    @food = food
+    @meal_time = meal_time
+  end
+  def name
+    @name
+  end
+  def food
+    @food
+  end
+  def meal_time
+    @meal_time
+  end
+
+end
