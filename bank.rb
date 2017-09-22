275844844a4f465089179f5a77122e9dfca03863
Argir Argirov <dj.giro@gmail.com>
2017-09-22 13:19:24 -0400

    Bank account done

---
+++ bank.rb
@@ -0,0 +1,34 @@
+class BankAccount
+  # READERS
+  def balance
+    @balance
+  end
+  def interest_rate
+    @interest_rate
+  end
+
+  # WRITERS
+  def deposit=(amount)
+    @balance = amount
+  end
+  def withdraw=(amount)
+    @balance -= amount
+  end
+  def interest
+    @balance *= 1.1
+  end
+end
+
+account1 = BankAccount.new
+
+account1.deposit = 534
+
+puts account1.balance
+
+account1.withdraw = 100
+
+puts account1.balance
+
+account1.interest
+
+puts account1.balance
