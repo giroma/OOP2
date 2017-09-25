d3a5215c966fafc506b50ddd16a2e1a920093494
Argir Argirov <dj.giro@gmail.com>
2017-09-25 14:54:03 -0400

    bank account done!

---
+++ sep25th/bank_account.rb
@@ -0,0 +1,63 @@
+class BankAccount
+  @@interest_rate = 1.01
+  @@accounts = []
+  #  CLASS METHODS
+  def self.create
+  new_account = BankAccount.new
+  @@accounts << new_account
+  return new_account
+  end
+  def self.total_funds
+    sum = 0
+    @@accounts.each do |object|
+      sum += object.balance
+    end
+    return sum
+  end
+  def self.interest_time
+    index = 0
+    @@accounts.each do |item|
+      item.reasign_balance(item.balance * @@interest_rate)
+      index += 1
+    end
+  end
+
+  def initialize
+    @balance = 0
+  end
+
+  def balance
+    @balance
+  end
+        # WRITERS
+  def deposit(amount)
+    @balance += amount
+  end
+  def withdraw(amount)
+    @balance -= amount
+  end
+  def reasign_balance(value)
+    @balance = value
+  end
+
+
+end
+
+my_account = BankAccount.create
+your_account = BankAccount.create
+# puts my_account.inspect
+puts my_account.balance # 0
+
+puts BankAccount.total_funds # 0
+my_account.deposit(200)
+your_account.deposit(1000)
+puts my_account.balance # 200
+puts your_account.balance # 1000
+puts BankAccount.total_funds # 1200
+BankAccount.interest_time
+puts my_account.balance # 202.0
+puts your_account.balance # 1010.0
+puts BankAccount.total_funds # 1212.0
+my_account.withdraw(50)
+puts my_account.balance # 152.0
+puts BankAccount.total_funds # 1162.0
